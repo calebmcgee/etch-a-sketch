bb255fe13b826d6fccf63b6357cc528b8fa4f004
Caleb <calebmcgee06@gmail.com>
2024-04-15 21:36:04 -0400

    Make func for squares to change color on hover

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 const container = document.querySelector('.container');
+const gridSizeButton = document.querySelector('.gridSizeButton');
 
-
-function makeGrid(rowLength,rowAmount) {
-    for (x=0; x < rowAmount; x++){
+function makeGrid(sideLength) {
+    let grid = document.createElement('div');
+    for (x=0; x < sideLength; x++){
         let gridRow = document.createElement('div');
         gridRow.classList.add('gridRow');
-        for (i=0; i< rowLength; i++) {
+        for (i=0; i< sideLength; i++) {
             let gridSquare = document.createElement('div');
             gridSquare.classList.add('gridSquare');
             gridRow.appendChild(gridSquare);
         }
-        container.appendChild(gridRow);
+        grid.appendChild(gridRow);
+    }
+    container.appendChild(grid);
+}
+makeGrid(10);
+function colorChange(event){
+    if (event.target.classList.value === 'gridSquare'){
+        event.target.style.backgroundColor = 'green';
     }
 }
-
-makeGrid(16,16);
+let gridSquare = document.querySelectorAll('.gridSquare');
+gridSquare.forEach((element)=> addEventListener('mouseover', (event) => {
+    colorChange(event);
+}));
 
